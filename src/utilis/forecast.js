6270c901906a20572e4cfb1ed7af0245f56d5c11
Rachit Singhal <rachitsinghal40@gmail.com>
2020-04-01 02:05:59 +0530

    test

--- src/utilis/forecast.js
+++ src/utilis/forecast.js
@@ -9,7 +9,7 @@ const forecast=(latitude,longitude,callback)=>{
             callback("Unable to find location",undefined)
         } else{
             callback(undefined,response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degress out. There is a ' + response.body.currently.precipProbability + '% chance of rain.')
-        }
+        }//
     }
     )
 }
